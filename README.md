afdf048923ee48d8fe72624de12f96d4f423c193
Dennis Meister <dennis.meister@bosch.com>
2022-09-28 14:26:12 +0200

    First velocitas-project-generator open source release
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

---
+++ README.md
@@ -0,0 +1,45 @@
+# velocitas-project-generator-npm
+
+[![License: Apache](https://img.shields.io/badge/License-Apache-yellow.svg)](http://www.apache.org/licenses/LICENSE-2.0)
+
+## Main Usage
+
+At the moment the scope of velocitas-project-generator is for [digital.auto](https://digitalauto.netlify.app/).
+The velocitas-project-generator takes python prototype code from the digital.auto playground, adapts it and makes it compatible with the
+[eclipse-velocitas vehicle app python template](https://github.com/eclipse-velocitas/vehicle-app-python-template).
+
+## Integrate in your repository
+
+Install from the command line:
+```bash
+npm install @eclipse-velocitas/velocitas-project-generator
+```
+
+Install via package.json:
+```json
+"@eclipse-velocitas/velocitas-project-generator": "^1.0.0"
+```
+
+Manual integration:
+clone it, transpile it and link it into you repositories node_modules folder
+
+```bash
+git clone https://github.com/eclipse-velocitas/velocitas-project-generator-npm.git
+cd velocitas-project-generator-npm
+tsc
+cd path/to/your/repo
+npm link path/to/velocitas-project-generator-npm
+```
+
+## Usage in Code
+```javascript
+import { ProjectGenerator } from "@eclipse-velocitas/velocitas-project-generator";
+
+const generator = new ProjectGenerator(OWNER, REPO, TOKEN);
+await generator.run(BASE64_CODE_SNIPPET, APP_NAME);
+```
+
+## Contribution
+- [GitHub Issues](https://github.com/eclipse-velocitas/velocitas-project-generator-npm/issues)
+- [Mailing List](https://accounts.eclipse.org/mailing-list/velocitas-dev)
+- [Contribution](https://eclipse-velocitas.github.io/velocitas-docs/docs/contribution/)
---
+++ src/project-generator.ts
@@ -0,0 +1,284 @@
+// Copyright (c) 2022 Robert Bosch GmbH
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License, Version 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import axios from 'axios';
+import { Buffer } from 'buffer';
+import { ProjectGeneratorError } from './project-generator-error';
+import { StatusCodes } from 'http-status-codes';
+
+const GITHUB_API_URL = 'https://api.github.com/repos';
+const PYTHON_TEMPLATE_URL = `${GITHUB_API_URL}/eclipse-velocitas/vehicle-app-python-template`;
+
+const CONTENT_ENCODINGS = { utf8: 'utf-8', base64: 'base64' };
+
+const GIT_DATA_TYPES = { blob: 'blob', tree: 'tree', commit: 'commit' };
+const GIT_DATA_MODES = {
+    fileBlob: '100644',
+    executableBlob: '100755',
+    subdirectoryTree: '040000',
+    submoduleCommit: '160000',
+    symlinkPathBlob: '120000',
+};
+const DEFAULT_REPOSITORY_DESCRIPTION = 'Template generated from eclipse-velocitas';
+const DEFAULT_COMMIT_MESSAGE = 'Update content with digital.auto code';
+
+/**
+ * Initialize a new `ProjectGenerator` with the given `options`.
+ *
+ * @param {Object} [options]
+ * @return {ProjectGenerator} which can be used to generate a repository.
+ * @public
+ */
+export class ProjectGenerator {
+    private repositoryPath;
+    private requestConfig;
+    /**
+     * Parameter will be used to call the GitHub API as follows:
+     * https://api.github.com/repos/OWNER/REPO
+     *
+     * PAT or Oauth token with scope for atleast:
+     * user, public_repo, repo, notifications, gist
+     * @param {string} owner
+     * @param {string} repo
+     * @param {string} authToken as PAT or Oauth Token
+     */
+    constructor(private owner: string, private repo: string, private authToken: string) {
+        this.requestConfig = {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/vnd.github+json',
+                Authorization: `Bearer ${this.authToken}`,
+            },
+        };
+        this.repositoryPath = `${GITHUB_API_URL}/${this.owner}/${this.repo}`;
+    }
+
+    /**
+     * @param {string} codeSnippet Base64 encoded playground code snippet.
+     * @param {string} appName Name of the VehicleApp.
+     * @throws {ProjectGeneratorError}
+     */
+    public async run(codeSnippet: string, appName: string): Promise<number> {
+        try {
+            await this.generateRepo();
+            await this.updateContent(appName, codeSnippet);
+            return StatusCodes.OK;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    private async generateRepo(): Promise<number> {
+        try {
+            await axios.post(
+                `${PYTHON_TEMPLATE_URL}/generate`,
+                {
+                    owner: this.owner,
+                    name: this.repo,
+                    description: DEFAULT_REPOSITORY_DESCRIPTION,
+                    include_all_branches: false,
+                    private: true,
+                },
+                this.requestConfig
+            );
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                throw new ProjectGeneratorError(error);
+            } else {
+                throw error;
+            }
+        }
+        const responseStatus = await this.checkRepoAvailability();
+        await this.setWorkflowPermission(false);
+        return responseStatus;
+    }
+
+    private async checkRepoAvailability(): Promise<number> {
+        let retries = 0;
+        let success = false;
+        let responseStatus: StatusCodes = StatusCodes.INTERNAL_SERVER_ERROR;
+        const maxRetries = 20;
+
+        while (retries < maxRetries && !success) {
+            try {
+                const response = await axios.get(`${this.repositoryPath}/contents`, this.requestConfig);
+                responseStatus = response.status;
+                success = true;
+                return responseStatus;
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    console.log(`Check #${retries + 1} if Repository is generated failed. Retrying.`);
+                    responseStatus = error.response?.status as number;
+                } else {
+                    throw error;
+                }
+            }
+            retries++;
+        }
+        return responseStatus;
+    }
+
+    private async setWorkflowPermission(isEnabled: boolean): Promise<boolean> {
+        try {
+            await axios.put(
+                `${this.repositoryPath}/actions/permissions`,
+                {
+                    enabled: isEnabled,
+                },
+                this.requestConfig
+            );
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    private async createBlob(fileContent: string): Promise<string> {
+        try {
+            const response = await axios.post(
+                `${this.repositoryPath}/git/blobs`,
+                {
+                    content: fileContent,
+                    encoding: CONTENT_ENCODINGS.base64,
+                },
+                this.requestConfig
+            );
+            const blobSha = response.data.sha;
+            return blobSha;
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                throw new ProjectGeneratorError(error);
+            } else {
+                throw error;
+            }
+        }
+    }
+
+    private async updateTree(sha1: string, sha2: string): Promise<any> {
+        try {
+            const baseTreeSha = await this.getBaseTreeSha();
+            const newTreeSha = await this.createNewTreeSha(sha1, sha2, baseTreeSha);
+            const mainBranchSha = await this.getMainBranchSha();
+            const newCommitSha = await this.createCommitSha(mainBranchSha, newTreeSha);
+            await this.setWorkflowPermission(true);
+            await this.updateMainBranchSha(newCommitSha);
+            return StatusCodes.OK;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    private async updateContent(appName: string, codeSnippet: string): Promise<any> {
+        const getAppManifestContentResponse = await axios.get(`${this.repositoryPath}/contents/app/AppManifest.json`, this.requestConfig);
+        const appManifestContentData = getAppManifestContentResponse.data.content;
+        let decodedContent = JSON.parse(
+            Buffer.from(appManifestContentData, CONTENT_ENCODINGS.base64 as BufferEncoding).toString(
+                CONTENT_ENCODINGS.utf8 as BufferEncoding
+            )
+        );
+        decodedContent[0].Name = appName;
+        const updateContentString = JSON.stringify(decodedContent, null, 4);
+        const encodedUpdateContent = Buffer.from(updateContentString, CONTENT_ENCODINGS.utf8 as BufferEncoding).toString(
+            CONTENT_ENCODINGS.base64 as BufferEncoding
+        );
+        const appManifestBlobSha = await this.createBlob(encodedUpdateContent);
+        const mainPyBlobSha = await this.createBlob(codeSnippet);
+        await this.updateTree(appManifestBlobSha, mainPyBlobSha);
+    }
+
+    private async getBaseTreeSha(): Promise<string> {
+        try {
+            const response = await axios.get(`${this.repositoryPath}/git/trees/main`, this.requestConfig);
+            const baseTreeSha = response.data.sha;
+            return baseTreeSha;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    private async createNewTreeSha(blobSha1: string, blobSha2: string, baseTreeSha: string): Promise<string> {
+        try {
+            const response = await axios.post(
+                `${this.repositoryPath}/git/trees`,
+                {
+                    tree: [
+                        {
+                            path: 'app/AppManifest.json',
+                            mode: GIT_DATA_MODES.fileBlob,
+                            type: GIT_DATA_TYPES.blob,
+                            sha: blobSha1,
+                        },
+                        {
+                            path: 'app/src/main.py',
+                            mode: GIT_DATA_MODES.fileBlob,
+                            type: GIT_DATA_TYPES.blob,
+                            sha: blobSha2,
+                        },
+                    ],
+                    base_tree: baseTreeSha,
+                },
+                this.requestConfig
+            );
+            const newTreeSha = response.data.sha;
+            return newTreeSha;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    private async getMainBranchSha(): Promise<string> {
+        try {
+            const response = await axios.get(`${this.repositoryPath}/git/refs/heads/main`, this.requestConfig);
+            const mainBranchSha = response.data.object.sha;
+            return mainBranchSha;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    private async createCommitSha(mainBranchSha: string, newTreeSha: string): Promise<string> {
+        try {
+            const response = await axios.post(
+                `${this.repositoryPath}/git/commits`,
+                {
+                    tree: newTreeSha,
+                    message: DEFAULT_COMMIT_MESSAGE,
+                    parents: [mainBranchSha],
+                },
+                this.requestConfig
+            );
+            const commitSha = response.data.sha;
+            return commitSha;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    private async updateMainBranchSha(newCommitSha: string): Promise<any> {
+        try {
+            const response = await axios.patch(
+                `${this.repositoryPath}/git/refs/heads/main`,
+                {
+                    sha: newCommitSha,
+                },
+                this.requestConfig
+            );
+            const patchResponse = response.data;
+            return patchResponse;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
