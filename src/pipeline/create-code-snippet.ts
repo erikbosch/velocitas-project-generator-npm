5b7cac4a46b51cedc9a579e463aac65c49d7273b
Serhii Korchynskyi <serhii.korchynskyi@etas.com>
2024-01-10 14:27:04 +0100

    Fix/license_headers (#30)
    * Update license-header and pre-commit-action.yaml
    
    * Added linter verification
    
    * Fix self review findings
    
    * Fix self review findings
    
    * Fix some issues
    
    * Update ci.yml
    
    * Fix
    
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>
    
    * Remove license header from .gitignore
    
    * Update license header to 2024
    
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>
    
    ---------
    
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>
    Co-authored-by: Dennis Meister <dennis.meister@bosch.com>
    Co-authored-by: BjoernAtBosch <Bjoern.Hornburg@de.bosch.com>

--- src/pipeline/create-code-snippet.ts
+++ src/pipeline/create-code-snippet.ts
@@ -1,3 +1,17 @@
+// Copyright (c) 2023-2024 Contributors to the Eclipse Foundation
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License, Version 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { CodeContext } from '../code-converter';
 import { INDENTATION, VELOCITAS } from '../utils/codeConstants';
 import { createMultilineStringFromArray, indentCodeSnippet, variableConditionCheck } from '../utils/helpers';
@@ -14,7 +28,7 @@ export class CreateCodeSnippetForTemplateStep extends PipelineStep {
         context.codeSnippetForTemplate = `${indentCodeSnippet(VELOCITAS.ON_START, INDENTATION.COUNT_CLASS)}\n${indentCodeSnippet(
             this.adaptCodeBlocksToVelocitasStructure(createMultilineStringFromArray(context.codeSnippetStringArray)),
             INDENTATION.COUNT_METHOD
-            )}`;
+        )}`;
     }
     private changeMemberVariables(context: CodeContext) {
         context.variableNames.forEach((variableName: string) => {
@@ -34,7 +48,7 @@ export class CreateCodeSnippetForTemplateStep extends PipelineStep {
                     context.codeSnippetStringArray[index] = stringElement.replace(variableName, `self.${variableName}`);
                 }
 
-                if (variableConditionCheck(stringElement,variableName)) {
+                if (variableConditionCheck(stringElement, variableName)) {
                     context.codeSnippetStringArray[index] = stringElement.replace(variableRegex(variableName), `self.${variableName}`);
                 }
             });
